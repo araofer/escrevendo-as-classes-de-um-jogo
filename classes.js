a98f62c0f330024a175f7254d64071088d08c9b8
araofer <araofer33@gmail.com>
2025-11-26 17:46:13 -0300

    Upload

---
+++ classes.js
@@ -0,0 +1,27 @@
+// Classe que representa um herói
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+
+        // Define o ataque de acordo com o tipo
+        if (this.tipo === "ninja") {
+            ataque = "shuriken";
+        }
+
+        // Exibe a mensagem final
+        console.log("O ninja atacou usando shuriken");
+
+    }
+}
+
+// Criando o herói 
+const heroi = new Heroi("Yoshimitsu", 100, "ninja");
+
+// Chamando o método atacar
+heroi.atacar();
